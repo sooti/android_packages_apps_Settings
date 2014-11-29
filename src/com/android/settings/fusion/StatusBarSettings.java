ce67c6cb72d145c6f9fbf92154755a7771d44fe0
Dragon <dragon7780@hotmail.nl>
2014-11-29 14:56:08 +0100

    Statusbar show battery percent
    Signed-off-by: Dragon <dragon7780@hotmail.nl>

--- src/com/android/settings/fusion/StatusBarSettings.java
+++ src/com/android/settings/fusion/StatusBarSettings.java
@@ -17,12 +17,12 @@
 package com.android.settings.fusion;
 
 import android.os.Bundle;
-import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
 
@@ -33,14 +33,30 @@ import com.android.settings.Utils;
 public class StatusBarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
+    private static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+
+    private SwitchPreference mStatusBarShowBatteryPercent;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.fusion_statusbar_settings);
+
+        mStatusBarShowBatteryPercent = (SwitchPreference) findPreference(STATUS_BAR_SHOW_BATTERY_PERCENT);
+        mStatusBarShowBatteryPercent.setOnPreferenceChangeListener(this);
+        int statusBarShowBatteryPercent = Settings.System.getInt(getContentResolver(),
+                STATUS_BAR_SHOW_BATTERY_PERCENT, 0);
+        mStatusBarShowBatteryPercent.setChecked(statusBarShowBatteryPercent != 0);
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mStatusBarShowBatteryPercent) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getContentResolver(), STATUS_BAR_SHOW_BATTERY_PERCENT,
+                    value ? 1 : 0);
+            return true;
+        }
         return false;
     }
 
