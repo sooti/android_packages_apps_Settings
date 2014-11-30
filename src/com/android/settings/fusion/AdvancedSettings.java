4639c128561c809626c19d44ce64a3dc7bf2f10a
Dragon <dragon7780@hotmail.nl>
2014-11-30 17:48:00 +0100

    MediaScanner behavior on boot (3/3)
    Lets you enable/disable/ask (with a notification) media scanning on boot.
    Can speed up boot and reduce initial boot lag from MediaScanner
    
    Signed-off-by: Dragon <dragon7780@hotmail.nl>

--- src/com/android/settings/fusion/AdvancedSettings.java
+++ src/com/android/settings/fusion/AdvancedSettings.java
@@ -20,25 +20,37 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
 import java.util.List;
 
-public class AdvancedSettings extends SettingsPreferenceFragment {
+public class AdvancedSettings extends SettingsPreferenceFragment
+    implements OnPreferenceChangeListener {
 
     private static final String PREF_DEVICESETTINGS_APP = "devicesettings_app";
+    private static final String PREF_MEDIA_SCANNER_ON_BOOT = "media_scanner_on_boot";
 
     private PreferenceScreen mDeviceSettingsApp;
+    private ListPreference mMsob;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.fusion_advanced_settings);
+
+        mMsob = (ListPreference) findPreference(PREF_MEDIA_SCANNER_ON_BOOT);
+        mMsob.setValue(String.valueOf(Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.MEDIA_SCANNER_ON_BOOT, 0)));
+        mMsob.setSummary(mMsob.getEntry());
+        mMsob.setOnPreferenceChangeListener(this);
 
         mDeviceSettingsApp = (PreferenceScreen) findPreference(PREF_DEVICESETTINGS_APP);
 
@@ -58,4 +70,20 @@ public class AdvancedSettings extends SettingsPreferenceFragment {
         }
         return false;
     }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        String value = (String) newValue;
+        if (preference == mMsob) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.MEDIA_SCANNER_ON_BOOT,
+                    Integer.valueOf(value));
+
+            mMsob.setValue(String.valueOf(value));
+            mMsob.setSummary(mMsob.getEntry());
+            return true;
+        }
+        return false;
+    }
+
 }
