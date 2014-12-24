f27481a668867d5b2c2e2a95bb1d629c0b256186
Dragon <dragon7780@hotmail.nl>
2014-12-24 14:06:19 +0100

    Custom system animations: Temp disable the "prevent app overrides" option
    This causes weird issues with recents
    
    Signed-off-by: Dragon <dragon7780@hotmail.nl>

--- src/com/android/settings/fusion/AnimationControls.java
+++ src/com/android/settings/fusion/AnimationControls.java
@@ -23,7 +23,7 @@ import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
-import android.preference.CheckBoxPreference;
+import android.preference.SwitchPreference;
 import android.provider.Settings;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.widgets.AnimBarPreference;
@@ -45,7 +45,7 @@ public class AnimationControls extends SettingsPreferenceFragment
     private static final String TASK_MOVE_TO_FRONT = "task_move_to_front";
     private static final String TASK_MOVE_TO_BACK = "task_move_to_back";
     private static final String ANIMATION_DURATION = "animation_duration";
-    private static final String ANIMATION_NO_OVERRIDE = "animation_no_override";
+    //private static final String ANIMATION_NO_OVERRIDE = "animation_no_override";
     private static final String WALLPAPER_OPEN = "wallpaper_open";
     private static final String WALLPAPER_CLOSE = "wallpaper_close";
     private static final String WALLPAPER_INTRA_OPEN = "wallpaper_intra_open";
@@ -62,7 +62,7 @@ public class AnimationControls extends SettingsPreferenceFragment
     ListPreference mWallpaperIntraOpen;
     ListPreference mWallpaperIntraClose;
     AnimBarPreference mAnimationDuration;
-    CheckBoxPreference mAnimNoOverride;
+    //SwitchPreference mAnimNoOverride;
 
     private int[] mAnimations;
     private String[] mAnimationsStrings;
@@ -86,9 +86,9 @@ public class AnimationControls extends SettingsPreferenceFragment
             mAnimationsNum[i] = String.valueOf(mAnimations[i]);
         }
 
-        mAnimNoOverride = (CheckBoxPreference) findPreference(ANIMATION_NO_OVERRIDE);
+        /*mAnimNoOverride = (SwitchPreference) findPreference(ANIMATION_NO_OVERRIDE);
         mAnimNoOverride.setChecked(Settings.System.getBoolean(mContentRes,
-                Settings.System.ANIMATION_CONTROLS_NO_OVERRIDE, false));
+                Settings.System.ANIMATION_CONTROLS_NO_OVERRIDE, false));*/
 
         mActivityOpenPref = (ListPreference) findPreference(ACTIVITY_OPEN);
         mActivityOpenPref.setOnPreferenceChangeListener(this);
@@ -157,16 +157,16 @@ public class AnimationControls extends SettingsPreferenceFragment
         mAnimationDuration.setOnPreferenceChangeListener(this);
     }
 
-    @Override
+    /*@Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        if (preference == mAnimNoOverride) {
+        /*if (preference == mAnimNoOverride) {
             Settings.System.putBoolean(mContentRes,
                     Settings.System.ANIMATION_CONTROLS_NO_OVERRIDE,
                     mAnimNoOverride.isChecked());
             return true;
         }
         return false;
-    }
+    }*/
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
